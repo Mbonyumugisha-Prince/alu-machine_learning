f9093e1fe777308891ccbb3e392131efeacedf76
Mbonyumugisha-Prince <p.mbonyumug@alustudent.com>
2026-01-17 12:59:30 +0200

    commit

---
+++ math/plotting/4-frequency.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python3
+import numpy as np
+import matplotlib.pyplot as plt
+
+np.random.seed(5)
+student_grades = np.random.normal(68, 15, 50)
+
+# Define bins every 10 units from 0 to 100
+bins = np.arange(0, 101, 10)
+
+# Plot the histogram
+plt.hist(student_grades, bins=bins, edgecolor='black')
+plt.xlabel('Grades')
+plt.ylabel('Number of Students')
+plt.title('Project A')
+plt.show()
