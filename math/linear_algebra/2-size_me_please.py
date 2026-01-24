32770dcaa059514f9eb1c1da7ad031f6d12c1d48
Mbonyumugisha-Prince <p.mbonyumug@alustudent.com>
2026-01-23 23:46:22 -0800

    commit

--- math/linear_algebra/2-size_me_please.py
+++ math/linear_algebra/2-size_me_please.py
@@ -1,6 +1,20 @@
 #!/usr/bin/env python3
+"""
+This module contains a function to calculate the shape of a nested matrix.
+"""
+
 def matrix_shape(matrix):
+    """
+    Calculate the shape of a nested matrix.
+
+    Args:
+        matrix (list): A list (possibly nested) representing a matrix.
+
+    Returns:
+        list: A list of integers representing the shape of the matrix.
+    """
     shape = [len(matrix)]
     if isinstance(matrix[0], list):
         shape.extend(matrix_shape(matrix[0]))
     return shape
+
