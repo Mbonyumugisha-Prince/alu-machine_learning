7ccf5972c7e220a3e864a01e81fc91e507f96370
Mbonyumugisha-Prince <p.mbonyumug@alustudent.com>
2026-01-31 03:47:12 -0800

    commit

---
+++ math/advanced_linear_algebra/5-definiteness.py
@@ -0,0 +1,44 @@
+#!/usr/bin/env python3
+"""Module to calculate the definiteness of a square matrix."""
+
+import numpy as np
+
+
+def definiteness(matrix):
+    """Calculates the definiteness of a square NumPy matrix."""
+    # Check type
+    if not isinstance(matrix, np.ndarray):
+        raise TypeError("matrix must be a numpy.ndarray")
+
+    # Check for empty or non-square
+    if matrix.size == 0 or len(matrix.shape) != 2 or matrix.shape[0] != matrix.shape[1]:
+        return None
+
+    # Symmetrize to avoid floating point asymmetry issues
+    if not np.allclose(matrix, matrix.T):
+        return None
+
+    try:
+        # Compute eigenvalues
+        eigvals = np.linalg.eigvals(matrix)
+    except np.linalg.LinAlgError:
+        return None
+
+    pos = np.all(eigvals > 0)
+    pos_semi = np.all(eigvals >= 0) and not pos
+    neg = np.all(eigvals < 0)
+    neg_semi = np.all(eigvals <= 0) and not neg
+
+    if pos:
+        return "Positive definite"
+    elif pos_semi:
+        return "Positive semi-definite"
+    elif neg:
+        return "Negative definite"
+    elif neg_semi:
+        return "Negative semi-definite"
+    elif np.any(eigvals > 0) and np.any(eigvals < 0):
+        return "Indefinite"
+    else:
+        return None
+
