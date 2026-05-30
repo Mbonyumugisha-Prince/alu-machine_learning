9e3762b99f68ebb6ad2cf42291654e116ce5ee28
Mbonyumugisha-Prince <p.mbonyumug@alustudent.com>
2026-05-30 15:41:28 +0200

    fixing the errors

--- supervised_learning/classification/1-neuron.py
+++ supervised_learning/classification/1-neuron.py
@@ -3,30 +3,31 @@
 
 import numpy as np
 
+
 class Neuron:
-    """Class that defines as single neuron performing binary classification."""
+    """Class that defines a single neuron performing binary classification."""
 
     def __init__(self, nx):
-        """Initialize Neuron"""
+        """Initialize Neuron."""
         if not isinstance(nx, int):
             raise TypeError("nx must be an integer")
         if nx < 1:
             raise ValueError("nx must be a positive integer")
-        self.W = np.random.randn(1, nx)
-        self.b = 0
-        self.A = 0
+        self.__W = np.random.randn(1, nx)
+        self.__b = 0
+        self.__A = 0
 
     @property
-    def W(self) :
+    def W(self):
         """Get the weight vector."""
         return self.__W
-    
+
     @property
-    def b(self) :
+    def b(self):
         """Get the bias."""
         return self.__b
-    
+
     @property
-    def A(self) :
+    def A(self):
         """Get the activated output."""
         return self.__A
