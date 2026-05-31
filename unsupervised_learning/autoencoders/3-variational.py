95e071253a8a8a337663972b549a7543778a2457
Mbonyumugisha-Prince <p.mbonyumug@alustudent.com>
2026-05-31 18:32:16 +0200

    fixing the error

--- unsupervised_learning/autoencoders/3-variational.py
+++ unsupervised_learning/autoencoders/3-variational.py
@@ -2,7 +2,6 @@
 """Variational Autoencoder"""
 
 import tensorflow.keras as keras
-import tensorflow.keras.backend as K
 
 
 def autoencoder(input_dims, hidden_layers, latent_dims):
@@ -16,7 +15,6 @@ def autoencoder(input_dims, hidden_layers, latent_dims):
     Returns:
         encoder, decoder, auto
     """
-    # Encoder
     inputs = keras.Input(shape=(input_dims,))
     x = inputs
     for nodes in hidden_layers:
@@ -28,15 +26,16 @@ def autoencoder(input_dims, hidden_layers, latent_dims):
     def sampling(args):
         """Reparameterization trick."""
         mean, log_var = args
-        epsilon = K.random_normal(shape=(K.shape(mean)[0], latent_dims))
-        return mean + K.exp(log_var / 2) * epsilon
+        epsilon = keras.backend.random_normal(
+            shape=(keras.backend.shape(mean)[0], latent_dims)
+        )
+        return mean + keras.backend.exp(log_var / 2) * epsilon
 
     z = keras.layers.Lambda(
         sampling, output_shape=(latent_dims,)
     )([z_mean, z_log_var])
     encoder = keras.Model(inputs, [z, z_mean, z_log_var])
 
-    # Decoder
     dec_inputs = keras.Input(shape=(latent_dims,))
     x = dec_inputs
     for nodes in reversed(hidden_layers):
@@ -44,20 +43,20 @@ def autoencoder(input_dims, hidden_layers, latent_dims):
     dec_outputs = keras.layers.Dense(input_dims, activation='sigmoid')(x)
     decoder = keras.Model(dec_inputs, dec_outputs)
 
-    # Full autoencoder
     z_out, z_mean_out, z_log_var_out = encoder(inputs)
     outputs = decoder(z_out)
     auto = keras.Model(inputs, outputs)
 
-    # VAE loss = reconstruction (binary cross-entropy summed) + KL divergence
-    reconstruction_loss = K.sum(
-        K.binary_crossentropy(inputs, outputs), axis=-1
+    reconstruction_loss = keras.backend.sum(
+        keras.backend.binary_crossentropy(inputs, outputs), axis=-1
     )
-    kl_loss = -0.5 * K.sum(
-        1 + z_log_var_out - K.square(z_mean_out) - K.exp(z_log_var_out),
+    kl_loss = -0.5 * keras.backend.sum(
+        1 + z_log_var_out
+        - keras.backend.square(z_mean_out)
+        - keras.backend.exp(z_log_var_out),
         axis=-1
     )
-    auto.add_loss(K.mean(reconstruction_loss + kl_loss))
+    auto.add_loss(keras.backend.mean(reconstruction_loss + kl_loss))
     auto.compile(optimizer='adam')
 
     return encoder, decoder, auto
